27388d48430171ef2ddd26fbf1daef8f648c1535
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2019-07-28 22:07:01 -0300

    Adicionada parte relacionada ao departamento do vendedor

--- ProjetoWeb_SistemaDeVendas/Controllers/SellersController.cs
+++ ProjetoWeb_SistemaDeVendas/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProjetoWeb_SistemaDeVendas.Models;
+using ProjetoWeb_SistemaDeVendas.Models.ViewModels;
 using ProjetoWeb_SistemaDeVendas.Service;
 
 namespace ProjetoWeb_SistemaDeVendas.Controllers
@@ -11,9 +12,12 @@ namespace ProjetoWeb_SistemaDeVendas.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
-        public SellersController(SellerService sellerService)
+        private readonly DepartmentService _departmentService;
+
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
         public IActionResult Index()
         {
@@ -23,7 +27,9 @@ namespace ProjetoWeb_SistemaDeVendas.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel{ Departments =  departments };
+            return View(viewModel);
         }
 
         [HttpPost]
---
+++ ProjetoWeb_SistemaDeVendas/Service/DepartmentService.cs
@@ -0,0 +1,22 @@
+﻿using ProjetoWeb_SistemaDeVendas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjetoWeb_SistemaDeVendas.Service
+{
+    public class DepartmentService
+    {
+        private readonly ProjetoWeb_SistemaDeVendasContext _context;
+
+        public DepartmentService(ProjetoWeb_SistemaDeVendasContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
