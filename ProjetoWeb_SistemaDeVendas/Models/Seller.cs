f460eccdeb1b3f54d08e52bfe054299df7337a5d
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2019-07-30 18:01:26 -0300

    Melhorias na formatação de datas e valores numéricos

--- ProjetoWeb_SistemaDeVendas/Models/Seller.cs
+++ ProjetoWeb_SistemaDeVendas/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace ProjetoWeb_SistemaDeVendas.Models
@@ -8,8 +9,17 @@ namespace ProjetoWeb_SistemaDeVendas.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        
+        [DataType(DataType.Date)]
+        [Display(Name = "Birth Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Base Salary (U$)")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
