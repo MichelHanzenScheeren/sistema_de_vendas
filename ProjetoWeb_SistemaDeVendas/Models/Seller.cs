6c3e2e9a6b0ce841d76d5c3b8969cfa46f1a2312
Michel Hanzen Scheeren <michelhanzenscheeren@gmail.com>
2019-07-30 18:36:40 -0300

    Validação do Create e Edit Seller

--- ProjetoWeb_SistemaDeVendas/Models/Seller.cs
+++ ProjetoWeb_SistemaDeVendas/Models/Seller.cs
@@ -8,19 +8,26 @@ namespace ProjetoWeb_SistemaDeVendas.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} is required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-        
+
+        [Required(ErrorMessage = "{0} is required")]
         [DataType(DataType.Date)]
         [Display(Name = "Birth Date")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
-        [Display(Name = "Base Salary (U$)")]
+        [Required(ErrorMessage = "{0} is required")]
+        [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; }
